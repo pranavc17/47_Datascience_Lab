b77a34b3f7c1e3a3ea67b6c454b89a0ca07c26f1
Bavithakv <79898409+Bavithakv@users.noreply.github.com>
2021-12-18 13:57:01 +0530

    Create 11.py

---
+++ MATRIX/11.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.array(([1,2,3],[4,5,6],[7,8,9]))
+b=np.array(([11,12,13],[2,4,6],[1,1,1]))
+print("first matrix = \n",a)
+print("second matrix = \n",b)
+print("sum of matrices\n",np.add(a,b))
+print("subtract of matrices\n",np.subtract(a,b))
+print("division of matrices\n",np.divide(a,b))
